05169ad969b5df1a57209817247965787668d324
evaldopoeta <mentefelizterapias@gmail.com>
2026-02-09 18:37:36 -0300

    Refactor main.tsx to use React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index'; // Seu painel administrativo
+import ClientView from './pages/ClientView'; // <--- IMPORTAR
 
-createRoot(document.getElementById("root")!).render(<App />);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* ROTA MÁGICA DO CLIENTE 👇 */}
+        <Route path="/projeto/:id" element={<ClientView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
